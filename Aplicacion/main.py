b749d42e4afe33791b1b9f0ae56cf2102112b986
GabrielCornejoB <gabrielcorbot@gmail.com>
2021-06-04 18:35:45 -0500

    Cambios UI graficos más que todo

--- Aplicacion/main.py
+++ Aplicacion/main.py
@@ -1,16 +1,16 @@
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
 Window.size = (360, 780)
+
 
 class MyApp(MDApp):
     def build(self):
         self.title = "CorneApp"
         self.theme_cls.primary_palette = "DeepPurple"
+        self.theme_cls.accent_palette = "Amber"
         screen = Builder.load_file("main.kv")
         return screen
 
