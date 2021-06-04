ee68c6ade1fddf3705ace9260d1fbcf9b2197477
GabrielCornejoB <gabrielcorbot@gmail.com>
2021-06-04 11:06:19 -0500

    Creación clase helpers y codigo base de main

---
+++ Aplicacion/main.py
@@ -0,0 +1,14 @@
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+
+Window.size = (360, 780)
+
+
+class MiApp(MDApp):
+    def build(self):
+        pass
+
+
+MiApp().run()
